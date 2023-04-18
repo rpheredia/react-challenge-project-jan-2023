07ef035ff1aac1272fe40bc1af321e0ca0e0bc5b
Rona Heredia <rpheredia@gmail.com>
2023-04-18 14:52:41 -0700

    feat: display email in nav bar

--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 import "./nav.css";
 
 const Nav = (props) => {
+    const auth = useSelector((state) => state.auth);
     return (
         <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
+            <Link to={"/view-orders"} className="nav-link">
+                <div className="nav-link-style">
+                    <label className="nav-label">Welcome {auth.email || 'Unknown!'}</label>
+                </div>
+            </Link>
+            <Link to={"/order"} className="nav-link" id="middle-link">
                 <div className="nav-link-style">
                     <label className="nav-label">Order Form</label>
                 </div>
